726f4486a76cedc4338651c04390e8fde85e5bc6
1337kid <vasanthrthanal@gmail.com>
2023-04-24 10:40:10 +0530

    v1.2.3

--- core/msgs.py
+++ core/msgs.py
@@ -1,8 +1,38 @@
 input_msgs=['Primary gradient colour in hex: ','Secondary gradient colour in hex: ',
+'Header text font colour in hex: ','Footer text font colour in hex: ',
 'Menubox selection background colour in hex: ','Menubox selection font colour: ',
 'GRUB font colour in hex: ','GRUB countdown colour in hex: ']
 
-noice_theme_extra={'menuboxbg':['Menu box background colour in hex: '],'polygon':['Topright polygon primary gradient colour in hex: ','Topright polygon secondary gradient colour in hex: '],
+noice_template_extra={'menuboxbg':['Menu box background colour in hex: '],
+'polygon':['Topright polygon primary gradient colour in hex: ','Topright polygon secondary gradient colour in hex: '],
 'ellipse':['Bottomright ellipse primary gradient colour in hex: ','Bottomright ellipse secondary gradient colour in hex: '],
 'circle':['Topleft circle primary gradient colour in hex: ','Topleft circle secondary gradient colour in hex: '],
 'triangle':['Bottomleft triangle backgound colour in hex: ']}
+
+banner = '''                                                
+o    o         o               .oPYo.  .oPYo. o    o  .oPYo. 
+8b   8                         8    8  8   `8 8    8  8   `8 
+8`b  8 .oPYo. o8 .oPYo. .oPYo. 8      o8YooP' 8    8 o8YooP' 
+8 `b 8 8    8  8 8    ' 8oooo8 8   oo  8   `b 8    8  8   `b 
+8  `b8 8    8  8 8    . 8.     8    8  8    8 8    8  8    8 
+8   `8 `YooP'  8 `YooP' `Yooo' `YooP8  8    8 `YooP'  8oooP' 
+..:::..:.....::..:.....::.....::....8 :..:::..:.....::......:
+:::::::: @1337kid ::::::::::::::::::8 ::::::: v1.2.3 ::::::::'''
+
+menulist = '''
+Available presets
+~~~~~~~~~~~~~~~~~
+>> Kewl Template
+[1] Chocolate	 [4] VioBlue
+[2] Lightlime    [5] Wildfire
+[3] TheSky
+
+>> Noice Template
+[6] Noice
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+[0] Custom'''
+
+template_menu = '''
+Availabe Templates
+[1] Kewl
+[2] Noice'''
