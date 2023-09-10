62c4f55823e41f166a27d16271a90dd176fb109d
1337kid <45532566+1337kid@users.noreply.github.com>
2023-09-10 08:12:06 -0400

    preset update

---
+++ core/msgs.py
@@ -0,0 +1,8 @@
+input_msgs=['Primary gradient colour in hex: ','Secondary gradient colour in hex: ',
+'GRUB selection background colour in hex: ','GRUB selection font colour: ',
+'GRUB font colour in hex: ','GRUB countdown colour in hex: ']
+
+noice_theme_extra={'polygon':['Topright polygon primary gradient colour in hex: ','Topright polygon secondary gradient colour in hex: '],
+'ellipse':['Bottomright ellipse primary gradient colour in hex: ','Bottomright ellipse secondary gradient colour in hex: '],
+'circle':['Topleft circle primary gradient colour in hex: ','Topleft circle secondary gradient colour in hex: '],
+'triangle':['Bottomleft triangle backgound colour in hex: ']}
